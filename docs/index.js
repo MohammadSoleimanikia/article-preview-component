bf63c2ad058b905abbdd015a5d34fe90bac3cae3
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-08-13 10:11:30 +0330

    add mobile version

---
+++ docs/index.js
@@ -0,0 +1,14 @@
+const btnLight=document.getElementById('btnLight');
+const btnDark=document.getElementById('btnDark');
+const content = document.getElementById('content');
+
+btnLight.addEventListener('click',()=>{
+    if (content.classList.contains('hidden')){
+        content.classList.remove('hidden');
+    }
+})
+btnDark.addEventListener('click',()=>{
+    if (!content.classList.contains('hidden')){
+        content.classList.add('hidden');
+    }
+})
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./docs/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        "Manrope":['Manrope']
+      }
+    },
+  },
+  plugins: [],
+}
+
