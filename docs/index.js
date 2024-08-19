8765a7db780dd6a1e7d4116dc0d6c2776b1fa963
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-08-19 12:19:23 +0330

    add two mode to tooltip btn

--- docs/index.js
+++ docs/index.js
@@ -1,26 +1,30 @@
-const btnLight=document.getElementById('btnLight');
-const btnDark=document.getElementById('btnDark');
-const content = document.getElementById('content');
+const btnLight = document.getElementById("btnLight");
+const btnDark = document.getElementById("btnDark");
+const content = document.getElementById("content");
 
 const tooltipLink = document.getElementById("tooltip-link");
 const tooltipBox = document.querySelector(".tooltip-box");
+const imgElement = document.querySelector(".tooltip-img");
+btnLight.addEventListener("click", () => {
+  content.classList.toggle("hidden");
+});
 
-btnLight.addEventListener('click',()=>{
-    content.classList.toggle('hidden');
-})
-
-btnDark.addEventListener('click',()=>{
-    if (!content.classList.contains('hidden')){
-        content.classList.add('hidden');
-    }
-})
-
-
-
+btnDark.addEventListener("click", () => {
+  if (!content.classList.contains("hidden")) {
+    content.classList.add("hidden");
+  }
+});
 
 // Check if elements exist to avoid errors
 if (tooltipLink && tooltipBox) {
   tooltipLink.addEventListener("click", () => {
     tooltipBox.classList.toggle("hide");
+    tooltipLink.classList.toggle("shareBtnDark");
   });
 }
+
+tooltipLink.addEventListener("click", function () {
+  imgElement.src = imgElement.src.includes("assets/imgs/icon-share.svg")
+    ? "assets/imgs/icon-share-light.svg"
+    : "assets/imgs/icon-share.svg";
+});
